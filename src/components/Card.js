52814057916fd6ea4ed69d20c4ab56459ffe1253
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-14 12:14:01 -0300

    tercero

--- src/components/Card.js
+++ src/components/Card.js
@@ -19,7 +19,7 @@ function Card({ imageSource, title, text, url }) {
       <div className="card-body text-light">
         <h4 className="card-title">{title}</h4>
         <p className="card-text text-secondary">{text}</p>
-        <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank" rel="noopener noreferrer"> 
+        <a href={url} className="btn btn-outline-secondary rounded-0" target="_blank" rel="noopener noreferrer"> {/* Cambiar el link */}
           Go to this page
         </a>
       </div>
