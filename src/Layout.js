8e3b0a8ba5f1183fff0059c4c1bb1d557834dd61
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-17 18:30:24 -0300

    prueba 3

--- src/Layout.js
+++ src/Layout.js
@@ -1,4 +1,4 @@
-import { Oulet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -8,7 +8,7 @@ const Layout = () => {
         <>
         <div>
             <Navbar />
-            <Oulet />
+            <Outlet />
             <Footer />
         </div>
         </>
