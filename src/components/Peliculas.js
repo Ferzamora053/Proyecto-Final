85d8926ef50701c50deee653a813d5a23b211382
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-17 19:42:48 -0300

    cambio peliculas

--- src/components/Peliculas.js
+++ src/components/Peliculas.js
@@ -1,83 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Pelicula from "./Pelicula";
+import axios from "axios";
 
-import image1 from "../images/image1.jpg";
-import image2 from '../images/image2.jpg'
-import image3 from '../images/image3.jpg'
-import image4 from '../images/image4.jpg'
-import image5 from '../images/image5.jpg'
-import image6 from '../images/image6.jpg'
-import image7 from '../images/image7.jpg'
-import image8 from '../images/image8.jpg'
-
-
-const peliculas = [
-  {
-    id: 1,
-    title: "El asesino",
-    image: image1,
-    url: "#",
-    text: ""
-  },
-  {
-    id: 2,
-    title: "Oppenheimer",
-    image: image2,
-    url: "#",
-    text: ""
-
-  },
-  {
-    id: 3,
-    title: "The Marvels",
-    image: image3,
-    url: "#",
-    text: ""
-
-  },
-  {
-    id: 4,
-    title: "Loki",
-    image: image4,
-    url: "#",
-    text: ""
-
-  },
-  {
-    id: 5,
-    title: "Ataque de los Titanes",
-    image: image5,
-    url: "#",
-    text: ""
-
-  },
-  {
-    id: 6,
-    title: "Five Nights at Freddy's",
-    image: image6,
-    url: "infopeli",
-    text: ""
-
-  },
-  {
-    id: 7,
-    title: "The Creator",
-    image: image7,
-    url: "#",
-    text: ""
-
-  },
-  {
-    id: 8,
-    title: "John Wick 4",
-    image: image8,
-    url: "#",
-    text: ""
-
-  },
-];
 
 function Peliculas() {
+  const [peliculas, setPeliculas] = useState([]);
+
+  useEffect(() => {
+    const apiKey = "c89fe48f27367d1ff270bdf4c51518ed";
+    const apiUrl = `https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`;
+    axios.get(apiUrl)
+    .then((response) => {
+      setPeliculas(response.data.results);
+    })
+
+  }, []);
+
   return (
     <div className="container d-flex justify-content-center align-items-center h-80">
       <div className="row">
