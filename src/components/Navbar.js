78eccd9f5cfa04ffb6d3f596e6cf57a189c3a6f2
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-17 20:56:21 -0300

    cambio navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../CSS/navbar.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
-function Navbar() {
+function NavbarC() {
   return (
-    <nav className="navbar navbar-expand-lg bg-custom">
-      <div className="container-fluid">
-        <a class="NPag"><Link to="/">CINEWEB</Link> </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNavAltMarkup"
-          aria-controls="navbarNavAltMarkup"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div className="navbar-nav">
-            <a className="nav-link active text-white" aria-current="page">
-              <Link to="/">Inicio</Link>
-            </a>
-            <a className="nav-link text-white">
-              <Link to="contacto">Contacto</Link>
-            </a>
-            <a className="nav-link text-white">
-              <Link to="infoequipo">Información del Equipo</Link>
-            </a>
-       
-          </div>
-        </div>
-      </div>
-    </nav>
+    <Navbar expand="lg" className="bg-custom">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic.navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#contacto">Contacto</Nav.Link>
+            <Nav.Link href="#equipo">Información del Equipo</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default NavbarC;
