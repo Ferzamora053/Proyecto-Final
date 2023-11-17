e369c6329b40bab67fdfd3a356dc9b335d0c30bf
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-17 18:31:46 -0300

    prueba 4

--- src/Layout.js
+++ src/Layout.js
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const Layout = () => {
-    retrun (
+    return (
         <>
         <div>
             <Navbar />
