054a7c019a3a23fc176dba4ec3999fc2fd4b1316
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-17 21:10:49 -0300

    links navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,13 +9,13 @@ function NavbarC() {
   return (
     <Navbar expand="lg" className="bg-custom">
       <Container fluid>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic.navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#contacto">Contacto</Nav.Link>
-            <Nav.Link href="#equipo">Información del Equipo</Nav.Link>
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="contacto">Contacto</Nav.Link>
+            <Nav.Link as={Link} to="infoequipo">Información del Equipo</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
