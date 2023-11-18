974648f6c7b3ef2e3c471cd25a3d437abba50f1c
Fernando Zamora <ferzamora@alumnos.uai.cl>
2023-11-17 21:01:53 -0300

    ultimo cambio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,7 @@ import Navbar from 'react-bootstrap/Navbar';
 function NavbarC() {
   return (
     <Navbar expand="lg" className="bg-custom">
-      <Container>
+      <Container fluid>
         <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic.navbar-nav">
