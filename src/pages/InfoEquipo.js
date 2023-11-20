cb9f431d0f305c2d890911969d51086539c9bed0
Arturo980 <111364051+Arturo980@users.noreply.github.com>
2023-11-20 18:04:08 -0300

    InfoEquipo.js

--- src/pages/InfoEquipo.js
+++ src/pages/InfoEquipo.js
@@ -26,10 +26,11 @@ function InfoEquipo () {
             <Card style={{ margin: 30 , color: "white"}}>
             <Card.Img variant="top" src={fer} />
             <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Fernando Zamora</Card.Title>
             <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            Profesional designado para la concepción y elaboración del diseño de la página web desempeña un papel fundamental en la 
+            configuración visual y estructural del sitio. Este rol implica la responsabilidad de 
+            asegurar que la estética del sitio web sea atractiva, cautivadora y acorde con los estándares contemporáneos de diseño.
             </Card.Text>
             </Card.Body>
             <Card.Footer>
@@ -39,10 +40,11 @@ function InfoEquipo () {
             <Card style={{ margin: 30 , color: "white"}}>
             <Card.Img variant="top" src={arturo} />
             <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Arturo Figueroa</Card.Title>
             <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
+            Profesional designado para llevar a cabo las funciones operativas de la página web desempeña un rol central en la implementación, 
+            supervisión y mantenimiento de las características interactivas y operativas del sitio. Este papel implica una responsabilidad fundamental 
+            en la garantía del funcionamiento efectivo de todas las herramientas y funcionalidades disponibles en el entorno digital.
             </Card.Text>
             </Card.Body>
             <Card.Footer>
@@ -52,11 +54,11 @@ function InfoEquipo () {
             <Card style={{ margin: 30 , color: "white"}}>
             <Card.Img variant="top" src={diego} />
             <Card.Body>
-            <Card.Title>Card title</Card.Title>
+            <Card.Title>Diego Espinoza</Card.Title>
             <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
+            Profesional designado para la elaboración de la estructura de la página web desempeña un papel crucial en la configuración y 
+            organización lógica del sitio. Este rol implica la responsabilidad de asegurar que la arquitectura del sitio sea meticulosamente 
+            planificada, presentando una disposición coherente y fácilmente accesible para los usuarios.
             </Card.Text>
             </Card.Body>
             <Card.Footer>
