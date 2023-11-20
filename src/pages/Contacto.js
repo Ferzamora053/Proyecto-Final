d8fbd51be9d0baa9b3ee49f3f3f858fc8724dc4e
Diego Espinoza <diego.espm911@gmail.com>
2023-11-20 15:17:59 -0300

    contacto e info

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -1,54 +1,132 @@
 import React from "react";
 import "../CSS/contacto.css";
-import fondocont from "../images/fondocont.jpg";
+import '../CSS/anular.css';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
 
 function Contacto() {
 
-    const fondoStyle = {
-        backgroundImage: `url(${fondocont})`,
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-    };
-
-    return (
-        <div lassName="container-fluid">
-            <div className="container fondo"style={fondoStyle}>
-                <div>
-                    <h1 className="text-center text-white">CineWeb</h1>
-                    <p className="text-white">
-                        ¡Bienvenidos a nuestra página web de cine, el lugar donde la magia del séptimo arte cobra vida! Aquí, nos enorgullece presentarles no solo una selección excepcional de películas, sino también una experiencia digital diseñada con pasión y dedicación.
-                    </p>
-                    <p className="text-white">
-                        <strong>Nuestra Historia del Cine:</strong> Sumérgete con nosotros en un viaje a través de las eras cinematográficas. Desde los primeros destellos de imágenes en blanco y negro hasta las producciones de vanguardia de la actualidad, nuestra página web está dedicada a exponer la rica y fascinante historia del cine. Descubre cómo esta forma de arte ha evolucionado, inspirado y conectado a personas de todo el mundo.
-                    </p>
-                    <p className="text-white">
-                        <strong>Misión:</strong> En el corazón de nuestra misión se encuentra el deseo de brindar experiencias cinematográficas excepcionales que perduren en la memoria de nuestros visitantes. Nos esforzamos por ofrecer una cuidadosa selección de películas que no solo entretengan, sino que también provoquen reflexión y emoción. Queremos ser el destino preferido de los amantes del cine, donde cada visita se traduzca en momentos inolvidables.
-                    </p>
-                    <p className="text-white">
-                        <strong>Visión:</strong> Imaginamos un futuro donde la magia del cine continúe siendo una fuente inagotable de inspiración y entretenimiento. Nuestra visión es crear un espacio donde la diversidad de voces y perspectivas en el cine se celebre y donde cada usuario se sienta parte de una comunidad apasionada. Buscamos ir más allá de ser simplemente un cine; aspiramos a ser un hogar para aquellos que comparten nuestra devoción por las historias bien contadas.
-                    </p>
-                    <p className="text-white">
-                        <strong>Valores:</strong> En nuestra página web, nos guiamos por valores fundamentales que creemos son esenciales para crear una experiencia auténtica y enriquecedora.
-                    </p>
-                    <ul className="text-white">
-                        <li><strong>Pasión por el Cine:</strong> Nos apasiona el cine en todas sus formas y expresiones, y buscamos compartir esa pasión con cada visitante.</li>
-                        <li><strong>Diversidad y Inclusión:</strong> Valoramos y celebramos la diversidad en todas sus formas, tanto en las películas que presentamos como en nuestra comunidad de espectadores.</li>
-                        <li><strong>Calidad y Excelencia:</strong> Nos esforzamos por ofrecer la más alta calidad en todo lo que hacemos, desde la selección de películas hasta la experiencia digital que ofrecemos a nuestros usuarios.</li>
+  return (
+    <Container fluid className="reset-card-styles">
+        <h1><p className="text-center fw-bold mt-2">CineWeb</p></h1>
+        <p className="text-center">¡Bienvenidos a nuestra página web de cine, el lugar donde la magia del séptimo 
+        arte cobra vida! Aquí, nos enorgullece presentarles no solo una selección excepcional de películas, 
+        sino también una experiencia digital diseñada con pasión y dedicación.</p>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh' }}>
+            <div className="row">
+                <div className=" col-sm-5 col-md-5" style={{ margin: "auto" }}>
+                    <h4 style={{ color: "#00008B"}} className="fw-bold">Valores:</h4>
+                    <ul>
+                        <li>En nuestra página web, nos guiamos por valores fundamentales que creemos son esenciales para crear una experiencia auténtica y enriquecedora:</li>
+                        <li>Pasión por el Cine: Nos apasiona el cine en todas sus formas y expresiones, y buscamos compartir esa pasión con cada visitante.</li>
+                        <li>Diversidad y Inclusión: Valoramos y celebramos la diversidad en todas sus formas, tanto en las películas que presentamos como en nuestra comunidad de espectadores.</li>
+                        <li>Calidad y Excelencia: Nos esforzamos por ofrecer la más alta calidad en todo lo que hacemos, desde la selección de películas hasta la experiencia digital que ofrecemos a nuestros usuarios.</li>
                     </ul>
-                    <p className="text-white">
-                        <strong>Experiencia del Usuario:</strong> Sabemos que su tiempo es valioso, y es por eso que hemos diseñado nuestra página web para que la exploración sea fácil, intuitiva y, sobre todo, divertida. Queremos que se sienta como en casa mientras navega por nuestra selección de películas, descubre detalles fascinantes sobre la historia del cine y se sumerge en la magia que solo el cine puede ofrecer.
-                    </p>
-                    <h1 className="text-center text-white mb-5">....</h1>
+                </div>
+                <div className=" col-sm-5 col-md-5">
+                <img
+                src="https://img.freepik.com/foto-gratis/vista-elementos-cine-3d_23-2150720822.jpg?size=626&ext=jpg&ga=GA1.1.1880011253.1700438400&semt=sph"
+                alt="Elementos de cine"
+                style={{ maxWidth: '100%', height: 'auto' }} // Estilo para hacer la imagen responsiva
+                />
                 </div>
             </div>
-            <h1 className="text-center text-black">Si quieres saber más de nosotros contactanos:</h1>
-
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '50vh' }}>
+            <Card border="primary" style={{ display: 'flex', flexDirection: 'row', width: '30rem', margin: 20 }}>
+            <div style={{ flex: 2, padding: '1rem' }}>
+            <Card.Body>
+                <Card.Title style={{ color: "#00008B"}} className="fw-bold">Nuestra Historia del Cine:</Card.Title>
+                <Card.Text>
+                Sumérgete con nosotros en un viaje a través de las eras cinematográficas. 
+                Desde los primeros destellos de imágenes en blanco y negro hasta las producciones de vanguardia de la actualidad, 
+                nuestra página web está dedicada a exponer la rica y fascinante historia del cine. Descubre cómo esta forma de arte ha evolucionado, 
+                inspirado y conectado a personas de todo el mundo logrando un sin fin de nuevas personas que llegan a este mundo.
+                </Card.Text>
+            </Card.Body>
+            </div>
+            </Card>
+            <Card border="primary" style={{ display: 'flex', flexDirection: 'row', width: '30rem', margin: 20 }}>
+            <div style={{ flex: 2, padding: '1rem' }}>
+            <Card.Body>
+                <Card.Title style={{ color: "#00008B"}} className="fw-bold" >Misión:</Card.Title>
+                <Card.Text>
+                En el corazón de nuestra misión se encuentra el deseo de brindar experiencias cinematográficas excepcionales 
+                que perduren en la memoria de nuestros visitantes. Nos esforzamos por ofrecer una cuidadosa selección de películas
+                 que no solo entretengan, sino que también provoquen reflexión y emoción. Queremos ser el destino preferido de los amantes del cine, 
+                 donde cada visita se traduzca en momentos inolvidables.
+                </Card.Text>
+            </Card.Body>
+            </div>
+            </Card>
+            <Card border="primary" style={{ display: 'flex', flexDirection: 'row', width: '30rem', margin: 20 }}>
+            <div style={{ flex: 2, padding: '1rem' }}>
+            <Card.Body>
+                <Card.Title style={{ color: "#00008B"}} className="fw-bold">Visión:</Card.Title>
+                <Card.Text>
+                Imaginamos un futuro donde la magia del cine continúe siendo una fuente inagotable de inspiración y entretenimiento. 
+                Nuestra visión es crear un espacio donde la diversidad de voces y perspectivas en el cine se celebre y donde cada usuario 
+                se sienta parte de una comunidad apasionada. Buscamos ir más allá de ser simplemente un cine; aspiramos a ser un hogar para aquellos 
+                que comparten nuestra devoción por las historias bien contadas.
+                </Card.Text>
+            </Card.Body>
+            </div>
+            </Card>
         </div>
 
-       
-            
+        <p className="text-center">Si quieres saber más de nosotros, contactanos...</p>
+        <h1><p className="text-center fw-bold mt-4">Contacto</p></h1>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '60vh' }}>
+        <Form>
+        <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridName">
+        <Form.Label>Nombre</Form.Label>
+        <Form.Control type="text" placeholder="Enter your name" />
+        </Form.Group>
 
-    )
+        <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter your email" />
+        </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3" controlId="formGridSubject">
+        <Form.Label>Tema</Form.Label>
+        <Form.Control placeholder="Enter the subject" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridMessage">
+        <Form.Label>Mensaje</Form.Label>
+        <Form.Control as="textarea" rows={4} placeholder="Enter your message" />
+        </Form.Group>
+
+        <Row className="mb-3">
+        <Form.Group as={Col} controlId="formGridPhone">
+        <Form.Label>Telefono</Form.Label>
+        <Form.Control type="tel" placeholder="Enter your phone number" />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridCompany">
+        <Form.Label>Compañia</Form.Label>
+        <Form.Control type="text" placeholder="Enter your company name" />
+        </Form.Group>
+        </Row>
+
+        <Button variant="primary" type="submit">
+        Enviar
+        </Button>
+        </Form>
+        </div>
+
+
+    </Container>
+    
+  );
 }
 
 export default Contacto;
--- src/pages/InfoEquipo.js
+++ src/pages/InfoEquipo.js
@@ -1,8 +1,72 @@
 import React from "react";
 
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import CardGroup from 'react-bootstrap/CardGroup';
+import arturo from "../images/arturo.jpg";
+import diego from "../images/diego.jpg";
+import fer from "../images/fer.jpg";
+
 function InfoEquipo () {
     return (
-        <h1>HOLA</h1>
+
+        <Container fluid className="">
+            <div className="reset-card-styles">
+            <Card className="text-center mt-5">
+                <Card.Header>CineWeb</Card.Header>
+                <Card.Body>
+                <Card.Title>Nuestro Equipo</Card.Title>
+                <Card.Text>
+                Todos los integrantes que aportaron su granito de arena para que este proyecto sea posible.
+                </Card.Text>
+                </Card.Body>
+            </Card>
+            </div>
+            <CardGroup className="mt-5 mb-5" style={{ margin: 100 }}>
+            <Card style={{ margin: 30 , color: "white"}}>
+            <Card.Img variant="top" src={fer} />
+            <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Fernando Zamora</small>
+            </Card.Footer>
+            </Card>
+            <Card style={{ margin: 30 , color: "white"}}>
+            <Card.Img variant="top" src={arturo} />
+            <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+            This card has supporting text below as a natural lead-in to
+            additional content.{' '}
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Arturo Figueroa</small>
+            </Card.Footer>
+            </Card>
+            <Card style={{ margin: 30 , color: "white"}}>
+            <Card.Img variant="top" src={diego} />
+            <Card.Body>
+            <Card.Title>Card title</Card.Title>
+            <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This card has even longer content than the
+            first to show that equal height action.
+            </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+            <small className="text-muted">Diego Espinoza</small>
+            </Card.Footer>
+            </Card>
+            </CardGroup>
+        </Container>
+ 
+      
     );
 }
 
